635c9bdbb7b0b9d0208bf17244695859bf763e81
sserebrinka <lenaignatyeva77@gmail.com>
2025-03-03 17:19:56 +0300

    update test

--- test_main_page.py
+++ test_main_page.py
@@ -7,18 +7,14 @@ link = "http://selenium1py.pythonanywhere.com/"
 link_login = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)  
-    page.open()                         
-    page.go_to_login_page()
+    page = MainPage(browser, link)
+    page.open()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-def test_guest_should_be_login_page(browser):
-    page = LoginPage(browser, link_login)
-    page.open()
-    page.should_be_login_page()
 
         
