1f9d2b936d7cd97115ee188816630a5a821a2b6b
sserebrinka <lenaignatyeva77@gmail.com>
2025-03-04 11:45:30 +0300

    add solve_quiz_and_get_code_parametrize; find bug url

--- pages/product_page.py
+++ pages/product_page.py
@@ -53,4 +53,11 @@ class ProductPage(BasePage):
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+    
+    def solve_quiz_and_get_code_parametrize(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
         
--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from pages.product_page import ProductPage
+import pytest
 
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -9,4 +10,15 @@ def test_guest_can_add_product_to_basket(browser):
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
+    page.elements_matches()
+
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
+
+@pytest.mark.parametrize('link', urls)
+def test_guest_can_add_product_to_basket_find_bug(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code_parametrize()
     page.elements_matches()
