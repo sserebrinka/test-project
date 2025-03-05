7bce1c06222a7be8fd6aa3c32ae591b83adb965e
sserebrinka <lenaignatyeva77@gmail.com>
2025-03-05 13:16:48 +0300

    Improved test design; adding new tests

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -10,6 +11,12 @@ class BasePage():
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
+    def go_to_login_page(self):
+        self.browser.find_element(*BasePageLocators.LOGIN_LINK).click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def open(self):
         self.browser.get(self.url)
--- test_product_page.py
+++ test_product_page.py
@@ -3,7 +3,7 @@ from pages.product_page import ProductPage
 import pytest
 
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+# link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
 # def test_guest_can_add_product_to_basket(browser):
 #     page = ProductPage(browser, link)
@@ -23,21 +23,33 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?prom
 #     page.solve_quiz_and_get_code_2()
 #     page.elements_matches()
 
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
-    page.should_not_be_success_message()
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.add_product_to_basket()
+#     page.solve_quiz_and_get_code()
+#     page.should_not_be_success_message()
 
-def test_guest_cant_see_success_message(browser): 
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_not_be_success_message()
+# def test_guest_cant_see_success_message(browser): 
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_not_be_success_message()
 
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, link)
+# def test_message_disappeared_after_adding_product_to_basket(browser):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.add_product_to_basket()
+#     page.solve_quiz_and_get_code()
+#     page.should_be_disappearing_message()
+
+link_product = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    page = ProductPage(browser, link_product)
     page.open()
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
-    page.should_be_disappearing_message()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    page = ProductPage(browser, link_product)
+    page.open()
+    page.go_to_login_page()
